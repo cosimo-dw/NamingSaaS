01e3d38e4951ba9e92cf3b9cc374e0deaf58c486
cosimo-dw <cosimo.dw@gmail.com>
2013-11-24 23:17:01 +0800

    Heroku problems

--- db/migrate/20131124044239_create_histories.rb
+++ db/migrate/20131124044239_create_histories.rb
@@ -1,10 +1,11 @@
-class CreateHistories < ActiveRecord::Migration
+class CreateHistorys < ActiveRecord::Migration
   def change
-    create_table :histories do |t|
+    create_table :historys do |t|
       t.string :content
       t.integer :order_id
 
       t.timestamps
     end
+    add_index :historys, [:order_id, :created_at, :updated_at]
   end
 end
