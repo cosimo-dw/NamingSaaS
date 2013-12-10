266a454321ceb0ec712b5182835f1171687febc3
zhenxuan00 <610841656@qq.com>
2013-12-10 18:33:24 +0800

    translating_v5

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,21 @@ class User < ActiveRecord::Base
 
   before_save { email.downcase! }
 
-  before_create :create_remember_token
-
-  validates :name, presence: true, length: { maximum: 20 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+
+  before_create :create_remember_token
+  validates_presence_of :name, :message => "用户名不能为空！"
+  validates_presence_of :email, :message => "登录邮箱不能为空！"
+  #validates_presence_of :password, :message => 密码不能为空
+  #validates_presence_of :password_confirmation, :message => "密码验证不能为空！"
+  validates :name, length: { maximum: 20, :too_long => "用户名至多为二十个字符！"}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX, :message => "登录邮箱格式不正确！"},
+                    uniqueness: { case_sensitive: false, :message => "该邮箱已经注册！" }
+
+  validates :password, length: { minimum: 6, :too_short => "密码至少为六个字符！"}
+
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
