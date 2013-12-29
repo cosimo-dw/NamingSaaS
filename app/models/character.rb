d3c52b5afae696f18197f2c4aa873b9904b297e5
cosimo-dw <cosimo.dw@gmail.com>
2013-12-29 16:40:29 +0800

    modify character index view

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,10 +6,23 @@ class Character < ActiveRecord::Base
   end
 
   def self.all_structures()
-    collection = ['独']
-    (12272..12283).each do |code|
-      collection << [code].pack('U*')
-    end
+    collection = [%w(独体结构 独)]
+    collection << %w(⿰左右结构 ⿰)
+    collection << %w(⿱上下结构 ⿱)
+    collection << %w(⿲左中右结构 ⿲)
+    collection << %w(⿳上中下结构 ⿳)
+    collection << %w(⿴全包围结构 ⿴)
+    collection << %w(⿵上包围结构 ⿵)
+    collection << %w(⿶下包围结构 ⿶)
+    collection << %w(⿷左包围结构 ⿷)
+    collection << %w(⿸左上包围结构 ⿸)
+    collection << %w(⿹右上包围结构 ⿹)
+    collection << %w(⿺左下包围结构 ⿺)
+    collection << %w(⿻嵌套结构 ⿻)
+
+    #(12272..12283).each do |code|
+    #  collection << [code].pack('U*')
+    #end
     collection
   end
 
